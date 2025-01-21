1b2b6b023dae1029f0c9b9267505547dba507b05
May <abeillee@bk.ru>
2025-01-22 00:46:53 +0300

    chore: configure webpack

--- nonograms/webpack.config.js
+++ nonograms/webpack.config.js
@@ -1,9 +1,32 @@
+const { extension } = require('mime-types');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/script.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
+    entry: './src/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        assetModuleFilename: 'assets/[name][ext]',
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 9000,
+    },
 };
