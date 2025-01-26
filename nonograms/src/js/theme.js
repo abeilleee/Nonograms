0d8b6c36b29dfc6e9395705c155d862c4637f2ab
May <abeillee@bk.ru>
2025-01-26 16:16:56 +0300

    styles: change width of borders for field

--- nonograms/src/js/theme.js
+++ nonograms/src/js/theme.js
@@ -8,7 +8,7 @@ const themes = {
         '--main-color': '#ed0000',
         '--font-color': '#41403e',
         '--body-bg-color': '#ffffff',
-        '--body-bg-lines-color': 'd6d6d60',
+        '--body-bg-lines-color': '#d6d6d6',
         '--btn-bg-color': '#ffffff',
         '--btn-text-color': '#41403e',
     },
@@ -17,26 +17,40 @@ const themes = {
         '--main-color': '#5e0101',
         '--font-color': '#d4d3d3',
         '--body-bg-color': '#2a2a2a',
-        '--body-bg-lines-color': ' #393939',
+        '--body-bg-lines-color': '#393939',
         '--btn-bg-color': '#9c9b9b',
         '--btn-text-color': '#41403e',
     }
 }
 
-let isDarkTheme;
+let isDarkTheme = JSON.parse(localStorage.getItem('isDarkTheme'));
+changeTheme(isDarkTheme);
 
-const changeTheme = () => {
-    const theme = 'dark';
+if (!localStorage.getItem('isDarkTheme')) {
+    localStorage.setItem('isDarkTheme', false);
+}
+
+function changeTheme() {
+    const theme = isDarkTheme ? 'dark' : 'default';
     Object.entries(themes[theme]).forEach(([key, value]) => {
         root.style.setProperty(key, value);
     })
 }
 
+function themeHadler(e) {
+    e.preventDefault();
+    isDarkTheme = !isDarkTheme;
+    localStorage.setItem('isDarkTheme', isDarkTheme);
+    console.log(isDarkTheme);
+    changeTheme(isDarkTheme);
+}
 
-theme.addEventListener(('click'), (event) => {
-    console.log('hi');
-    changeTheme();
-});
+theme.addEventListener(('click'), (themeHadler));
+
+
+
+
+
 
 
 
